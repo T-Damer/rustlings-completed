81ee8bf5354d12dce8307d3986429fe307f28eb9
Daniil <dpancov64@gmail.com>
2022-08-21 23:41:52 +0300

    completed hashmaps, yeah, nah

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,8 +11,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -22,6 +20,7 @@ enum Fruit {
     Mango,
     Lychee,
     Pineapple,
+    Orange,
 }
 
 fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
@@ -34,9 +33,11 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Put new fruits if not already present. Note that you
-        // are not allowed to put any type of fruit that's already
-        // present!
+        if (basket.contains_key(&fruit)) {
+            continue;
+        } else {
+            basket.insert(fruit, 1);
+        }
     }
 }
 
